8a942f56515955477bb2814d12ec45bdb0ea42e2
Ted Benson <ted@steamship.com>
2022-01-25 14:25:46 -0500

    Pushing up to switch to Intel Mac..

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# emory-lecture
+# Emory Lecture
+
+These are notes & a set of Jupuyter notebooks intended for use with a few lectures at Emory about literature & computing.
+
+## Setup
+
+```
+python3 -m venv .venv
+source .venv/bin/activate
+python -m pip install -r requirements.txt
+```
---
+++ util.py
@@ -0,0 +1,17 @@
+import os
+def on_each_file(fn, name: str = None, display: bool = False):
+  ret = []
+  files = os.listdir('./data')
+  files.sort()
+  for filename in files:
+    if name is None or name in filename:
+      with open(os.path.join("data", filename), 'r') as f:
+        content = f.read()
+        output = fn(content)
+        if display:
+          print(filename)
+          print(output)
+          print()
+        ret.append(output)
+  return ret
+        
